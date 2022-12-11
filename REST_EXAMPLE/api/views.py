1ab0d62c2adfda3de64220bfe15d80832f5d2bb7
Tobias Horopciuc <thoropciuc@student.tgm.ac.at>
2022-12-11 17:01:52 +0100

    adadsadq

---
+++ REST_EXAMPLE/api/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def register(request):
+    params = {
+        "uname": request.GET.get('uname'),
+        "email": request.GET.get('email'),
+        "password": request.GET.get('password')
+    }
+    return HttpResponse(params.items())
